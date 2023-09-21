baa2204a4b2f240041d570ae61fdea2423388e3e
ThalesR2530 <thales2rosario@gmail.com>
2023-09-21 19:22:25 -0300

    initial

---
+++ index.js
@@ -0,0 +1,63 @@
+// const prompt = require("prompt-sync")();
+// let mensagem = "Olá, mundo!";
+// console.log(mensagem);
+
+// let resultado = 5+1;
+// console.log(resultado);
+
+// let idade = prompt("Digite sua Idade: ");
+// console.log("Sua idade é: " + idade);
+
+// const nota1 = parseFloat(prompt("Primeira Nota"));
+// const nota2 = parseFloat(prompt("Segunda Nota"));
+// const nota3 = parseFloat(prompt("Terceira Nota"));
+
+// const media = (nota1 + nota2 + nota3) / 3;
+
+// console.log(`A média é ${media.toFixed(2)}`);
+
+// let emails = {
+//        "Senhores alunos, o intervalo é às 21h",
+//        "Senhores alunos, o projeto é para o dia X",
+//        12
+//        "teste"
+//   }
+  
+//   console.log(emails[1])
+//   conso
+
+
+console.log("\nCalculadora\n")
+real num1
+real num2
+real result
+caracter sinal 
+
+console.log("Digite o sinal da operação:")
+prompt(sinal)
+
+escreva("Digite o num1:")
+ leia(num1)
+ escreva("Digite o num2:")
+ leia(num2)
+
+  escolha(sinal){
+  caso '+':    
+ escreva("O resultado é:", num1 + num2)
+ pare
+
+ caso '-':
+ escreva("o resultado é:", num1 - num2)
+ pare
+
+ caso '*':
+ escreva("o resultado é:", num1 * num2)
+ pare
+
+ caso'/':
+ escreva("o resultado é:", num1 / num2)
+ pare
+
+ caso contrario:
+ escreva("Operação Inválida!")
+ pare
