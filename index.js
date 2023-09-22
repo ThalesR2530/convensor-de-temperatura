1245117808a81a6f4cfeedfc839b37915414ebea
ThalesR2530 <thales2rosario@gmail.com>
2023-09-21 21:38:36 -0300

    21/09/2023

--- index.js
+++ index.js
@@ -1,63 +1,25 @@
-// const prompt = require("prompt-sync")();
-// let mensagem = "Olá, mundo!";
-// console.log(mensagem);
+function Conversor(){
+    
+let valor
+let temperatura
+let result
 
-// let resultado = 5+1;
-// console.log(resultado);
+console.log("Digite 'f' para Fahrenheit:\n")
+console.log("Digite 'c' para Celsius: \n")
+temperatura = prompt()
 
-// let idade = prompt("Digite sua Idade: ");
-// console.log("Sua idade é: " + idade);
+console.log("Digite o valor que deseja converter:\t")
+valor = parseFloat(prompt())
 
-// const nota1 = parseFloat(prompt("Primeira Nota"));
-// const nota2 = parseFloat(prompt("Segunda Nota"));
-// const nota3 = parseFloat(prompt("Terceira Nota"));
-
-// const media = (nota1 + nota2 + nota3) / 3;
-
-// console.log(`A média é ${media.toFixed(2)}`);
-
-// let emails = {
-//        "Senhores alunos, o intervalo é às 21h",
-//        "Senhores alunos, o projeto é para o dia X",
-//        12
-//        "teste"
-//   }
-  
-//   console.log(emails[1])
-//   conso
-
-
-console.log("\nCalculadora\n")
-real num1
-real num2
-real result
-caracter sinal 
-
-console.log("Digite o sinal da operação:")
-prompt(sinal)
-
-escreva("Digite o num1:")
- leia(num1)
- escreva("Digite o num2:")
- leia(num2)
-
-  escolha(sinal){
-  caso '+':    
- escreva("O resultado é:", num1 + num2)
- pare
-
- caso '-':
- escreva("o resultado é:", num1 - num2)
- pare
-
- caso '*':
- escreva("o resultado é:", num1 * num2)
- pare
-
- caso'/':
- escreva("o resultado é:", num1 / num2)
- pare
-
- caso contrario:
- escreva("Operação Inválida!")
- pare
+switch(temperatura){
+case 'f':
+result = valor - 32 * 5/9
+console.log("A conversão para graus Celsius é: ", result)
+break		 
+case 'c': 
+result = valor * 9/5 +32
+console.log("A conversão para graus Fahrenheit é: ", result)
+break
+ 
+   }
+}
